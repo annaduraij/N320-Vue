03bfd2e1c2f4ab15d125db4795d392a9e51c95a0
Jay Annadurai <122567409+annaduraij@users.noreply.github.com>
2023-11-16 06:40:59 -0500

    Update Vite.js Base URL

--- HW/8-vue/vite.config.js
+++ HW/8-vue/vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'http://localhost/Informatics/Projects/N320-Vue/HW/8-vue/dist/',
   plugins: [
     vue(),
   ],
