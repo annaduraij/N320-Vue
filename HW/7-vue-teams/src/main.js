f62d3e174acc0517405e43b2624124df2bb6d8f1
Jay Annadurai <122567409+annaduraij@users.noreply.github.com>
2023-11-30 08:58:07 -0500

    Vue Teams Homework - Initialize Multiple Views

---
+++ HW/7-vue-teams/src/main.js
@@ -0,0 +1,22 @@
+<!-- Auth: Jay Annadurai            -->
+<!-- Proj: N320: Vue - Teams        -->
+<!-- Date: 28 November 2023         -->
+<!-- Desc: Vue JS Loading File      -->
+
+import InputForm from './components/InputForm.vue';
+import Roster from './components/Roster.vue';
+import Member from './components/Member.vue';
+import SlotButton from './components/SlotButton.vue';
+import router from './router/index.js';
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+const app = createApp(App);
+app.component('input-form', InputForm);
+app.component('roster', Roster);
+app.component('member-info', Member);
+app.component('slot-button', SlotButton);
+
+app.use(router);
+app.mount('#app');
---
+++ HW/7-vue-teams/vite.config.js
@@ -0,0 +1,22 @@
+<!-- Auth: Jay Annadurai            -->
+<!-- Proj: N320: Vue - Teams        -->
+<!-- Date: 28 November 2023         -->
+<!-- Desc: Vue Configuration File   -->
+
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // base: 'http://localhost/Informatics/Projects/N320-Vue/HW/7-vue-teams/dist/',
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
